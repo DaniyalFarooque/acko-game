44cee36c7521909e0b23ed39c5f2c362fcf48570
Daniyal1999 <daniyal.farooque_int@acko.tech>
2024-12-13 00:21:37 +0530

    updated truck.jsx

--- src/components/models/misc/Truck.jsx
+++ src/components/models/misc/Truck.jsx
@@ -5,17 +5,59 @@ Files: truck.glb [170.7KB] > /Users/daniyal.farooque/Documents/JSProject/Mario-K
 */
 
 import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, Float } from '@react-three/drei'
+import { CuboidCollider, RigidBody } from "@react-three/rapier";
+import { useStore } from "../../store";
+import { useFrame } from '@react-three/fiber';
+
 export function Truck(props) {
   const { nodes, materials } = useGLTF('./models/misc/truck-transformed.glb')
+
+  const { actions } = useStore();
+  const ref = useRef();
+  const [scale, setScale] = React.useState(1);
+  const frames = useRef(0);
+  const body = useRef();
+  useFrame((state, delta) => {
+    const time = state.clock.getElapsedTime();
+    console.log('truck scale', scale)
+    console.log('ref', ref)
+
+    if(scale < 1 && frames.current > 0){
+      frames.current -= 1 * delta * 144;
+
+    }
+  }
+  );
+
   return (
-    <group {...props} dispose={null} scale={[100,100,100]} >
-      <mesh castShadow receiveShadow geometry={nodes.MM_Camion_ParteDelantera.geometry} material={materials.mobile_Camion} position={[0.187, 0.047, 0.052]} rotation={[Math.PI / 2, 0, -1.616]} scale={0.01} />
-      <mesh castShadow receiveShadow geometry={nodes.MM_Camion_CristalParabrisas.geometry} material={materials.MM_Camion1} position={[0.199, 0.062, 0.052]} rotation={[Math.PI / 2, 0, -1.616]} scale={0.01} />
-      <mesh castShadow receiveShadow geometry={nodes.MM_Camion_RuedaDelantera01.geometry} material={materials.Material} position={[0.205, 0.044, 0.042]} rotation={[Math.PI / 2, 0, -1.616]} scale={0.01} />
-      <mesh castShadow receiveShadow geometry={nodes.MM_Camion_ParteTrasera.geometry} material={materials.mobile_Camion_trans} position={[0.188, 0.047, 0.052]} rotation={[Math.PI / 2, 0, -1.616]} scale={0.01} />
+    <>
+    <RigidBody type="fixed" name="itemBox"
+      sensor
+      ref={body}
+      onIntersectionEnter={({other}) => {
+        if(other.rigidBodyObject.name === "player"){
+        
+        actions.setItem(props.item);
+        setScale(0);
+        frames.current = 400;
+        body.current.setEnabled(false);
+        }
+      }}
+      position={props.position}
+      colliders={false}
+    >
+    <CuboidCollider args={[1.5, 1.5, 1.5]} />
+    </RigidBody>
+    <group ref={ref} {...props} dispose={null} scale={[scale*100,scale*100,scale*100]} >
+      <mesh castShadow receiveShadow geometry={nodes.MM_Camion_ParteDelantera.geometry} material={materials.mobile_Camion} position={[0.187, 0.047, 0.052]} rotation={[Math.PI / 2, 0, -1.616]} scale={scale*0.01} />
+      <mesh castShadow receiveShadow geometry={nodes.MM_Camion_CristalParabrisas.geometry} material={materials.MM_Camion1} position={[0.199, 0.062, 0.052]} rotation={[Math.PI / 2, 0, -1.616]} scale={scale*0.01} />
+      <mesh castShadow receiveShadow geometry={nodes.MM_Camion_RuedaDelantera01.geometry} material={materials.Material} position={[0.205, 0.044, 0.042]} rotation={[Math.PI / 2, 0, -1.616]} scale={scale*0.01} />
+      <mesh castShadow receiveShadow geometry={nodes.MM_Camion_ParteTrasera.geometry} material={materials.mobile_Camion_trans} position={[0.188, 0.047, 0.052]} rotation={[Math.PI / 2, 0, -1.616]} scale={scale*0.01} />
     </group>
+      </>
   )
+
 
 }
 
